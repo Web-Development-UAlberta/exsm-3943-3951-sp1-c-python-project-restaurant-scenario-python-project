102baca7bd5a6210e8a18d9f3a02eae558a429f3
Ashray Sikka <ashray15.sikka@gmail.com>
2026-05-05 16:44:32 -0400

    Add views, forms, templates for auth, staff roles

--- Python_Project/restaurant/forms.py
+++ Python_Project/restaurant/forms.py
@@ -1,8 +1,37 @@
 from django import forms
-from .models import Restaurant
+from restaurant import models
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+
+User = get_user_model() # this will return me the 'user' model that we have configured in our database
 
 class RestaurantForm(forms.ModelForm):
     class Meta:
-        model = Restaurant
+        model = models.Restaurant
         fields = ['name', 'address', 'phone_number', 'opening_time', 'closing_time',
                   'latitude', 'longitude']
+        
+
+class CustomerSignUpForm(UserCreationForm):
+    phone_number = forms.CharField(max_length=10)
+    address = forms.CharField(widget=forms.Textarea)
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2', 'phone_number', 'address']
+        # ^ Using the inbuilt fields from AbstractUser class along with my manually set up fields
+
+
+class StaffSignUpForm(UserCreationForm):
+    phone_number = forms.CharField(max_length=10)
+    address = forms.CharField(widget=forms.Textarea)
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2', 'phone_number', 'address']
+        # ^ Using the inbuilt fields from AbstractUser class along with my manually set up fields
+
+class AddStaffForm(forms.ModelForm):
+    class Meta:
+        model = models.StaffInvite
+        fields = ['email', 'role']
