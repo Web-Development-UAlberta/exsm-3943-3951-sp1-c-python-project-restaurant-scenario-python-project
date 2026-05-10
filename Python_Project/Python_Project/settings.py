e0a731264af333eca1bf1fe53e0adeb2bf218b7a
Ashray Sikka <ashray15.sikka@gmail.com>
2026-05-10 17:43:51 -0400

    fix: pass DJANGO_SECRET_KEY secret to GitHub Actions

--- Python_Project/Python_Project/settings.py
+++ Python_Project/Python_Project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-load_dotenv()
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
