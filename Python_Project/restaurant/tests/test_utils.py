6b2c9a32c3ab9a65b45d9d2fdc467b892eaf7814
Ajay Paterson <ajay.paterson@gmail.com>
2026-05-11 21:53:01 -0600

    add test_utils.py with manual test notes

---
+++ Python_Project/restaurant/tests/test_utils.py
@@ -0,0 +1,6 @@
+# geocode_address and haversin distance are not unit tested here due to API dependency.
+# Validated manaully against know Calgary addresses relative to seed data for restaurant.  (300 Centre St, Calgary AB)
+
+# 225 8 Ave SE, Calgary, AB         4.1km => $5 delivery fee
+# 1750 Crowchild Trail NW, Calgary, AB      6.7km => $10 delivery fee
+# 11520 24 St SE, Calgary, AB       12.6km => delivery rejected
